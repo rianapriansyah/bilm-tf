58f900d785e108541a93f1a2f2d4f0bb3f3f9f1d
Rian Apriansyah <jmryan73@gmail.com>
2020-02-10 12:21:08 +0700

    update gpu number

--- bin/train_elmo.py
+++ bin/train_elmo.py
@@ -13,7 +13,7 @@ def main(args):
 
     # define the options
     batch_size = 128  # batch size for each GPU
-    n_gpus = 2
+    n_gpus = 1
 
     # number of tokens in training data (this for indonesia wikidump)
     n_train_tokens = 103693313
