1e17780210b64e9c26c19d64d331446c73ffe0e7
Rian Apriansyah <jmryan73@gmail.com>
2020-02-17 00:04:59 +0700

    update job file

--- train_elmo.sh
+++ train_elmo.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=train_elmo
 #SBATCH --output=train_elmo.txt
 #
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --time=4320:00
 
-srun --mpi=pmix python bin/train_elmo.py \
+srun python bin/train_elmo.py \
     --train_prefix='/home/m418662/elmo/train/*' \
     --vocab_file /home/m418662/vocabulary.txt \
     --save_dir /home/m418662/elmo/output
