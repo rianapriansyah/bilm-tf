850d17c5c68b2645939a6f206c784f62a1735076
Rian Apriansyah <jmryan73@gmail.com>
2020-03-10 19:47:46 +0700

    update

--- dump_weights.sh
+++ dump_weights.sh
@@ -7,5 +7,5 @@
 #SBATCH --time=4320:00
 
 srun python bin/dump_weights.py \
-	--save_dir /home/m418662/elmo/output
-	--outfile /home/m418662/elmo
+    --save_dir /home/m418662/elmo/output
+    --outfile /home/m418662/elmo
